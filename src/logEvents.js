532c3d3213fb29ecf12e7656a21da4f8d8fb7be2
deepak mishra <d.mishra@green-fusion.de>
2022-06-18 17:06:43 +0200

    logs added

--- src/logEvents.js
+++ src/logEvents.js
@@ -4,9 +4,6 @@ const fsPromises = require('fs').promises;
 const fs = require('fs');
 const path = require('path');
 
- const time = new Date();
- console.log("This is our time,date and day!", format(time, 'dd.mm.yyyy>HH:mm:ss, eeee' )) //"HH:mm,eeee"));
-
 const logEvents = async (message) => {
     const dateTime = `${format(new Date(), 'dd.mm.yyyy  HH:mm:ss')}`
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
