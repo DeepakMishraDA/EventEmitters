a15bf561d7151254108b0afb49812c3210a292f6
DeepakMishraDA <deepakvinodmishra@gmail.com>
2022-06-19 19:23:10 +0200

    http server added

--- logEvents.js
+++ logEvents.js
@@ -1,24 +1,31 @@
-const { format } = require('date-fns');
-const {v4:uuid} = require('uuid');
-const fsPromises = require('fs').promises;
-const fs = require('fs');
-const path = require('path');
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+const fsPromises = require("fs").promises;
+const fs = require("fs");
+const path = require("path");
 
 const logEvents = async (message) => {
-    const dateTime = `${format(new Date(), 'dd.mm.yyyy  HH:mm:ss')}`
-    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
-    console.log(logItem);
-    try {
-        if(!fs.existsSync(path.join(__dirname, 'logs'))){
-            await fsPromises.mkdir(path.join(__dirname,'logs'));
-            await fsPromises.appendFile(path.join(__dirname,'/logs/eventLog.odt'),logItem);
-            return;
-        }
-        await fsPromises.appendFile(path.join(__dirname,'/logs/eventLog.odt'),logItem);
-        return;
-    } catch(err){
-        console.log(err);
+  const date = `${format(new Date(), "dd.mm.yyyy")}`;
+  const time = `${format(new Date(), "HH:mm:ss")}`;
+  const logItem = `On this day ${date} at this time ${time} with an assigned id ${uuid()} a ${message} request has come. \n`;
+  console.log(logItem);
+  try {
+    if (!fs.existsSync(path.join(__dirname, "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "logs"));
+      await fsPromises.appendFile(
+        path.join(__dirname, "/logs/eventLog.odt"),
+        logItem
+      );
+      return;
     }
-}
+    await fsPromises.appendFile(
+      path.join(__dirname, "/logs/eventLog.odt"),
+      logItem
+    );
+    return;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = logEvents;
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const logEvents = require('./logEvents');
+const EventEmitter = require("events");
+const http = require("http");
 
-const EventEmitter = require('events');
-
-class OurEmitter extends EventEmitter {};
+const logEvents = require("./logEvents");
+class OurEmitter extends EventEmitter {}
 
 const myEmitter = new OurEmitter();
-myEmitter.on('log', (msg)=> logEvents(msg));
+// myEmitter.on("log", (msg) => logEvents(msg));
 
-setTimeout(()=>{
-    myEmitter.emit('log', 'Hello World!')
-}, 3000)
+// setTimeout(() => {
+//   myEmitter.emit("log", "Hello World!");
+// }, 3000);
+
+http
+  .createServer((req, res) => {
+    if (req.method === "GET") {
+      res.end("Hi Deepak");
+    } else {
+      logEvents(req.method);
+    }
+  })
+  .listen(3000);
